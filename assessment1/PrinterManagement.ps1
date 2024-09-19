3787b6a155c008fe080f4a1b73bb4a2ee4f8aab1
Spark-Aspire2 <aeu3000774@aspire2student.ac.nz>
2024-09-19 18:06:46 +1200

    add assessment 1

---
+++ assessment1/PrinterManagement.ps1
@@ -0,0 +1,29 @@
+﻿##########################################
+##### Spark Zheng 16/08/2024 #############
+##########################################
+
+# Install Printer Services
+Install-WindowsFeature -Name Print-Server -IncludeManagementTools 
+#Restart-Computer
+
+$PrinterDriverName = "Lexmark Universal v2 XL"
+$PrinterName="iac-printer"
+
+Add-PrinterDriver -Name $PrinterDriverName
+
+# add a printer on server side.
+Add-Printer -Name $PrinterName -DriverName $PrinterDriverName -PortName "LPT1:" -shared -ShareName "iac-printer" -Published
+#Set-Printer -Name $PrinterName -Shared $true
+
+
+#Get-Command -Module GroupPolicy
+New-GPO -Name PrinterPolicy 
+#New-GPLink -Name PrinterPolicy  -Target "ou=Sales,dc=spark,dc=com" -LinkEnabled Yes
+
+
+# Link GPO to OUs.
+$OUs=("Staff","Management","Account")
+
+foreach ($OU in $OUs) {
+    New-GPLink -Name PrinterPolicy  -Target "ou=$OU,dc=spark,dc=com" -LinkEnabled Yes
+}
---
+++ assessment1/WindowsBackup.ps1
@@ -0,0 +1,38 @@
+﻿##########################################
+##### Spark Zheng 16/08/2024 #############
+##########################################
+
+# Install Printer Services
+Install-WindowsFeature -Name Windows-Server-Backup -IncludeManagementTools 
+
+# Create a new Windows backup policy
+$Policy = New-WBPolicy
+
+# Configure the path of the files to backup
+$FileSpec = New-WBFileSpec -FileSpec "C:\Test4Backup"
+
+# Add the file path to the backup policy
+Add-WBFileSpec -Policy $Policy -FileSpec $FileSpec
+
+# Prepare the Credential to access backup storage
+$User = "administrator"
+$PWord = ConvertTo-SecureString -AsPlainText 'Aspire2' -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+
+# Configure the backup location
+$BackupLocation = New-WBBackupTarget -NetworkPath "\\VMWINDOWSSERVER\ShareFolder" -Credential $Credential
+
+# Add the target location to the backup policy
+Add-WBBackupTarget -Policy $Policy -Target $BackupLocation
+
+# Set backup time 
+Set-WBSchedule -Policy $Policy -Schedule 23:23
+
+# Add the backup policy to the backup server
+Set-WBPolicy -Policy $Policy
+
+# for once time backup execute start cmdlet directly.
+#Start-WBBackup -Policy $Policy
+
+
+
